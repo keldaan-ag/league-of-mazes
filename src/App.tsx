1c9f74ce73d5024a8542305afa608fa50e04b8b6
Keldaan <arnaudgregoire92@gmail.com>
2022-08-18 21:51:09 +0200

    table

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 import * as Colyseus from "colyseus.js";
-import { IGame } from './types';
+import { GameState, ICell, IBuildClick, Phase, Player, Transfer } from './types';
+import { ArraySchema } from '@colyseus/schema';
+import { Wait } from './components/Wait';
 
 function App() {
   let client = useRef(new Colyseus.Client('ws://localhost:4000'))
-  let room = useRef<Colyseus.Room<IGame>>()
+  let room = useRef<Colyseus.Room<GameState>>()
   let connected = useRef<boolean>(false)
-  let [gameState, setGameState] = useState<IGame|undefined>(undefined)
+  let [time, setTime] = useState<number>(0)
+  let [phase, setPhase] = useState<Phase>(Phase.WAIT)
+  let [players, setPlayers] = useState<Player[]>()
   
   useEffect(()=>{
     if(!room.current && !connected.current){
@@ -17,14 +21,51 @@ function App() {
 
   async function joinOrCreate(){
     room.current = await client.current.joinOrCreate('GameRoom')
-    room.current.onStateChange((state) => {
-      setGameState(state as IGame)
-    });
+    addStateListeners()
+  }
+
+  function cellClick(id: string,x: number,y: number){
+    room.current?.send(Transfer.BUILD_CLICK,{id,x,y} as IBuildClick)
+  }
+
+  function addStateListeners(){
+    if(room.current){
+      room.current.state.onChange = changes => {
+        changes.forEach(change=>{
+          if(change.field === 'time'){
+            setTime(change.value)
+          }
+          if(change.field === 'phase'){
+            setPhase(change.value)
+          }
+        })
+      }
+      (room.current.state.players as ArraySchema<Player>).onAdd = (player: Player, key: number) =>{
+        setPlayers(room.current!.state.players);
+        /*
+        player.onChange = (changes=>{
+          changes.forEach(change=>{
+            console.log(change.field)
+          })
+        });
+
+        player.maze.onChange = (changes =>{
+          changes.forEach(change=>{
+            console.log(change.field)
+          })
+        });
+        */
+        (player.maze.data as ArraySchema<ICell>).onChange = (cell) => {
+          setPlayers(room.current!.state.players)
+        }
+      }
+    }
   }
   
   return (
     <div className="App">
-      <p>{gameState?.time} {JSON.stringify(gameState?.players)}</p>
+      <p>Time {time} Phase {phase}</p>
+      <Wait players={players} cellClick={cellClick}/>
     </div>
   );
 }
